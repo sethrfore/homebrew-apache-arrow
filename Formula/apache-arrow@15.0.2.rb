4cfd60e9b13551efb697a3c26b6840debd003ecd
sethrfore <seth.fore@icloud.com>
2024-05-24 14:30:50 -0500

    SRF - Start Tracking

---
+++ Formula/apache-arrow@15.0.2.rb
@@ -0,0 +1,86 @@
+class ApacheArrowAT1502 < Formula
+  desc "Columnar in-memory analytics layer designed to accelerate big data"
+  homepage "https://arrow.apache.org/"
+  url "https://www.apache.org/dyn/closer.lua?path=arrow/arrow-15.0.2/apache-arrow-15.0.2.tar.gz"
+  mirror "https://archive.apache.org/dist/arrow/arrow-15.0.2/apache-arrow-15.0.2.tar.gz"
+  sha256 "abbf97176db6a9e8186fe005e93320dac27c64562755c77de50a882eb6179ac6"
+  license "Apache-2.0"
+  revision 3
+  head "https://github.com/apache/arrow.git", branch: "main"
+
+  keg_only "SRF - Avoid conflict with Arrow 16"
+
+  depends_on "boost" => :build
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "aws-sdk-cpp"
+  depends_on "brotli"
+  depends_on "bzip2"
+  depends_on "glog"
+  depends_on "grpc"
+  depends_on "llvm@17"
+  depends_on "lz4"
+  depends_on "openssl@3"
+  depends_on "protobuf"
+  depends_on "rapidjson"
+  depends_on "re2"
+  depends_on "snappy"
+  depends_on "thrift"
+  depends_on "utf8proc"
+  depends_on "zstd"
+  uses_from_macos "python" => :build
+
+  fails_with gcc: "5"
+
+  def install
+    # Work around an Xcode 15 linker issue which causes linkage against LLVM's
+    # libunwind due to it being present in a library search path.
+    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm@17"].opt_lib if DevelopmentTools.clang_build_version >= 1500
+
+    args = %W[
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+      -DARROW_ACERO=ON
+      -DARROW_COMPUTE=ON
+      -DARROW_CSV=ON
+      -DARROW_DATASET=ON
+      -DARROW_FILESYSTEM=ON
+      -DARROW_FLIGHT=ON
+      -DARROW_FLIGHT_SQL=ON
+      -DARROW_GANDIVA=ON
+      -DARROW_HDFS=ON
+      -DARROW_JSON=ON
+      -DARROW_ORC=ON
+      -DARROW_PARQUET=ON
+      -DARROW_PROTOBUF_USE_SHARED=ON
+      -DARROW_S3=ON
+      -DARROW_WITH_BZ2=ON
+      -DARROW_WITH_ZLIB=ON
+      -DARROW_WITH_ZSTD=ON
+      -DARROW_WITH_LZ4=ON
+      -DARROW_WITH_SNAPPY=ON
+      -DARROW_WITH_BROTLI=ON
+      -DARROW_WITH_UTF8PROC=ON
+      -DARROW_INSTALL_NAME_RPATH=OFF
+      -DPARQUET_BUILD_EXECUTABLES=ON
+      -GNinja
+    ]
+
+    args << "-DARROW_MIMALLOC=ON" unless Hardware::CPU.arm?
+
+    system "cmake", "-S", "cpp", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include "arrow/api.h"
+      int main(void) {
+        arrow::int64();
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-std=c++17", "-I#{include}", "-L#{lib}", "-larrow", "-o", "test"
+    system "./test"
+  end
+end
